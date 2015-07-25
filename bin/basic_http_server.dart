4473e5ab3281cd441b469f0e73316692e23a2519
Jack Murphy <jack@rightisleft.com>
2015-07-25 17:09:52 -0400

    test routes

--- bin/basic_http_server.dart
+++ bin/basic_http_server.dart
@@ -1,24 +1,34 @@
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_route/shelf_route.dart';
 import 'dart:io' show Platform;
 import 'dart:async' show runZoned;
 import 'package:path/path.dart' show join, dirname;
-import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_static/shelf_static.dart';
 
 void main() {
-  // Assumes the server lives in bin/ and that `pub build` ran
-  var pathToBuild = join(dirname(Platform.script.toFilePath()),
-      '..', 'build/web');
+  var pathToBuild = join(dirname(Platform.script.toFilePath()), '..', 'build/web');
+  var staticHandler = createStaticHandler(pathToBuild, defaultDocument: 'index.html');
 
-  var handler = createStaticHandler(pathToBuild,
-      defaultDocument: 'index.html');
+  Router aRouter = router();
+  aRouter.get('/item/{itemid}', (shelf.Request request) => new shelf.Response.ok('') );
+
+  var handler = const shelf.Pipeline()
+  .addMiddleware(shelf.logRequests())
+  .addHandler(staticHandler);
+
+  var zambia = new shelf.Cascade()
+  .add(aRouter.handler)
+  .add(handler)
+  .handler;
 
   var portEnv = Platform.environment['PORT'];
   var port = portEnv == null ? 9999 : int.parse(portEnv);
 
   runZoned(() {
-    io.serve(handler, '0.0.0.0', port);
+    io.serve(zambia, '0.0.0.0', port);
     print("Serving $pathToBuild on port $port");
-    io.serve(handler, '0.0.0.0', 8080);
   },
-  onError: (e, stackTrace) => print('Oh noes! $e $stackTrace'));
+  
+  onError: (e, stackTrace) => print('Top-Level-Error: $e $stackTrace'));
 }
